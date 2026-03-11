a84e0ad033a1e6a44bca877ad16a3059dc0514e2
luis cespedes <lxluxo23@gmail.com>
2026-03-11 15:34:28 -0300

    chore: configurar Metro para expo-sqlite en web
    - Añadir assetExts 'wasm' para resolver wa-sqlite.wasm
    - Añadir enhanceMiddleware con headers COEP/COOP para SharedArrayBuffer

---
+++ metro.config.js
@@ -0,0 +1,20 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+// expo-sqlite web: soporte para archivos .wasm
+config.resolver.assetExts.push('wasm');
+
+// Headers COEP/COOP para SharedArrayBuffer (expo-sqlite web)
+config.server = config.server ?? {};
+config.server.enhanceMiddleware = (middleware) => {
+  return (req, res, next) => {
+    res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    return middleware(req, res, next);
+  };
+};
+
+module.exports = config;
